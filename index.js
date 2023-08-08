ed6c7b8467cc9cd55d0ca628aff92d38bce927d5
konain1 <kaunainkasab@gmail.com>
2023-08-08 22:14:33 +0530

    callback

--- index.js
+++ index.js
@@ -6,5 +6,39 @@ const fs = require('fs')
 
 fs.writeFileSync('hello.txt',"hello from nodejs");
 
-// day one is overed
+const person = {
 
+    cat:'doora',
+
+    eat:()=>{
+        console.log(this.cat)
+    }
+    
+}
+let i = 0
+setInterval(() => {
+
+    console.log(++i)
+},1000);
+
+
+const fetchData = (callback)=>{
+    setTimeout(() => {
+        callback("hello world")
+    }, 1000);
+}
+
+
+
+
+// console.log(timer)
+fetchData( (cb)=>{
+    console.log("inside")
+    setTimeout(() => {
+        console.log(cb)
+    }, 5000);
+   
+})
+
+console.log("asyc")
+// person.eat()
